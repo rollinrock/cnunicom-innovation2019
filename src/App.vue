92d1a4971d03a6911f4044cee5ae9866c2c446da
rollinrock <caojing9111@outlook.com>
2019-06-02 23:21:09 +0800

    页面整体布局

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,88 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header class="header">
+        <el-row>
+          <!-- <el-col span=1><img class="brand-logo" src="./assets/bb.jpeg" height="30" width="30" ></el-col> -->
+          <el-col class="brand" :span="8">
+            <img class="brand-logo" src="./assets/cnunicom-logo.jpeg" height="40" width="40" >
+            <h3 class="brand-title">中国联通创新2019-金华分公司</h3>
+          </el-col>
+          <el-col :span="16">
+            <el-menu class="header-operations" mode="horizontal">
+              <el-menu-item index="1">登录</el-menu-item>
+              <el-menu-item index="2">登出</el-menu-item>
+
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-aside>
+        <el-menu default-active="1">
+          <el-menu-item index="1"><i class="el-icon-s-home"></i>首页</el-menu-item>
+          <el-menu-item index="2"><i class="el-icon-location"></i>宏站选址</el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main></el-main>
+      <!-- <el-footer><p>copyright</p></el-footer> -->
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  methods: {
+    showInfo() {
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
 }
+
+.header {
+  line-height: 60px;
+}
+.brand img{
+  float: left;
+}
+.brand h3 {
+  float: left;
+}
+
+.brand-logo {
+  padding-top: 12px;
+  /* padding-bottom: 10px; */
+}
+
+.header-operations {
+  float: right;
+}
+
+
+
+
+
+
+
+
 </style>
